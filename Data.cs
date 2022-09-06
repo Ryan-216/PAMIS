d441a659d22c52e9b160fafbaca44b25a0edbd62
红云 冉 <1192788287@qq.com>
2022-09-06 15:22:54 +0800

    Data

---
+++ Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Party_MS2
+{
+    class Data
+    {
+        public static string UID = "", UName = "";//登录用户的id和姓名
+    }
+}
