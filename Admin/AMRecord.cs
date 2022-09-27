218b765c8fe9e1210660c57b691fd0686f97d147
红云 冉 <1192788287@qq.com>
2022-09-27 13:42:31 +0800

    Revert "Revert "meeting""
    This reverts commit 8c8949a2800fa8edaee3aec7ac45af35e70c6320.

---
+++ Admin/AMRecord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Party_MS2
+{
+    public partial class AMRecord : Form
+    {
+        public AMRecord()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
